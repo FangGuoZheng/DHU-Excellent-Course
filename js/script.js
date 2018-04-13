68a3556d31b62175f452f6ccf119ceb6373efc4e
FangGuoZheng <1987356692@qq.com>
2018-04-13 15:32:55 +0800

    add page

--- js/script.js
+++ js/script.js
@@ -22,67 +22,67 @@ $(function() {
         }, {
             "url": "../plays/mooc-2.html",
             "img": "../images/course-2.jpg",
-            "title": "材料的电学性能",
+            "title": "材料的电学性能实验",
             "subtitle": "材料科学实验课程",
             "view": 88
         }, {
-            "url": "./details/detail01.html",
+            "url": "../plays/mooc-3.html",
             "img": "../images/course-3.jpg",
-            "title": "材料的拉伸强度测试~1",
+            "title": "材料的拉伸强度实验",
             "subtitle": "材料科学实验课程",
             "view": 84
         }, {
-            "url": "./details/detail01.html",
+            "url": "../plays/mooc-4.html",
             "img": "../images/course-4.jpg",
-            "title": "材料的热分析-完整实验",
+            "title": "材料的热分析实验",
             "subtitle": "材料科学实验课程",
             "view": 82
         }, {
-            "url": "./details/detail01.html",
+            "url": "../plays/mooc-5.html",
             "img": "../images/course-5.jpg",
-            "title": "材料的显微结构20160601",
+            "title": "材料的微观形态分析",
             "subtitle": "材料科学实验课程",
             "view": 104
         }, {
-            "url": "./details/detail01.html",
+            "url": "../plays/mooc-6.html",
             "img": "../images/course-6.jpg",
-            "title": "材料的硬度测试-1",
+            "title": "材料的硬度测试实验",
             "subtitle": "材料科学实验课程",
             "view": 106
         }, {
-            "url": "./details/detail01.html",
+            "url": "../plays/mooc-7.html",
             "img": "../images/course-7.jpg",
-            "title": "材料的粘度测定20131231",
+            "title": "材料的粘度测试实验",
             "subtitle": "材料科学实验课程",
             "view": 95
         }, {
-            "url": "./details/detail01.html",
+            "url": "../plays/mooc-8.html",
             "img": "../images/course-8.jpg",
-            "title": "材料光学性能测试",
+            "title": "材料的光学性能测试实验",
             "subtitle": "材料科学实验课程",
             "view": 98
         }, {
-            "url": "./details/detail01.html",
+            "url": "../plays/mooc-9.html",
             "img": "../images/course-9.jpg",
-            "title": "材料粒径测试",
+            "title": "材料的粒径测试实验",
             "subtitle": "材料科学实验课程",
             "view": 120
         }, {
-            "url": "./details/detail01.html",
+            "url": "../plays/mooc-10.html",
             "img": "../images/course-10.jpg",
-            "title": "固体表面的接触角测试",
+            "title": "固体材料表面的接触角测试实验",
             "subtitle": "材料科学实验课程",
             "view": 100
         }, {
-            "url": "./details/detail01.html",
+            "url": "../plays/mooc-11.html",
             "img": "../images/course-11.jpg",
-            "title": "测试~1",
+            "title": "材料的密度测试实验（测试部分）",
             "subtitle": "材料科学实验课程",
             "view": 101
         }, {
-            "url": "./details/detail01.html",
+            "url": "../plays/mooc-12.html",
             "img": "../images/course-12.jpg",
-            "title": "原理~1",
+            "title": "材料的密度测试实验（原理部分）",
             "subtitle": "材料科学实验课程",
             "view": 110
         }]
@@ -93,5 +93,4 @@ $(function() {
     $.each(list, function(key, val) {
         $('.course-list').append('<div class="col-sm-3 col-xs-6"><a href="' + val.url + '" target="_blank"><div class="course-card"><img alt="' + val.title + '" src="' + val.img + '"><div class="course-info"><p>' + val.title + '</p><span>' + val.subtitle + '</span></div><div class="course-state"><i class="fa fa-eye"> ' + val.view + '</i></div></div></a></div>')
     })
-
-})
+});
